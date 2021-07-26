bc2191ce992d3d7649079ce7810bc5a046989dc8
Bjorn Timmerhuis <bjorn@MacBook-Pro.local>
2021-07-26 12:48:18 +0200

    minor change to logout

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -3,16 +3,13 @@ import axios from 'axios';
 import cookie from 'cookie';
 
 export default async (req, res) => {
-  console.log(req);
   const apiRes = await axios.get(`${API_URL}users/logout`);
   const data = await apiRes.data;
-  // Destroy cookie
-  console.log(apiRes);
-  if (apiRes.statusCode === 200) {
+  if (data.message === 'success') {
     localStorage.removeItem('user');
     res.setHeader(
       'Set-Cookie',
-      cookie.serialize('JWT_token', 'abcdefgjklfskdlf', {
+      cookie.serialize('JWT_token', 'loggedout', {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development',
         expires: new Date(0), // in the past
