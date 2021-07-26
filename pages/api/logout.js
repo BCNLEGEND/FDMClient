966d1e2a64924942600c91c91b3d89aac415fc27
Bjorn Timmerhuis <bjorn@MacBook-Pro.local>
2021-07-26 13:34:08 +0200

    minor change to logout

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -3,23 +3,27 @@ import axios from 'axios';
 import cookie from 'cookie';
 
 export default async (req, res) => {
-  const apiRes = await axios.get(`${API_URL}users/logout`);
-  const data = await apiRes.data;
-  if (data.status === 'success') {
-    res.setHeader(
-      'Set-Cookie',
-      cookie.serialize('JWT_token', 'loggedout', {
-        httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development',
-        expires: new Date(0), // in the past
-        sameSite: 'strict',
-        path: '/',
-      })
-    );
-    res.status(200).json({ message: 'success' });
-  } else {
-    res
-      .status(401)
-      .json({ message: 'Something went wrong please try again later' });
+  try {
+    if (req.method === 'GET') {
+      const apiRes = await axios.get(`${API_URL}users/logout`);
+      const data = await apiRes.data;
+
+      if (data.status === 'success') {
+        res.setHeader(
+          'Set-Cookie',
+          cookie.serialize('JWT_token', data.token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== 'development',
+            expires: new Date(0), // in the past
+            sameSite: 'strict',
+            path: '/',
+          })
+        );
+
+        res.status(200).json({ message: 'successfully logged out' });
+      }
+    }
+  } catch (err) {
+    res.status(401).json(err);
   }
 };
