2a7a319d4ec8c2448cfb2f79b6b69b1f187d1526
Bjorn Timmerhuis <bjorn@MacBook-Pro.local>
2021-07-26 11:30:19 +0200

    minor change to logout

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -4,23 +4,24 @@ import cookie from 'cookie';
 
 export default async (req, res) => {
   try {
-    // const apiRes = await axios.get(`${API_URL}users/logout`);
-    // const data = await apiRes.data;
-    // // Destroy cookie
-    // console.log(data);
-    // if (data.status === 'success') {
-    localStorage.removeItem('user');
-    res.setHeader(
-      'Set-Cookie',
-      cookie.serialize('JWT_token', 'abcdefgjklfskdlf', {
-        httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development',
-        expires: new Date(0), // in the past
-        sameSite: 'strict',
-        path: '/',
-      })
-    );
-    res.status(200).json({ message: 'success' });
+    const apiRes = await axios.get(`${API_URL}users/logout`);
+    const data = await apiRes.data;
+    // Destroy cookie
+    console.log(apiRes);
+    if (data.status === 'success') {
+      localStorage.removeItem('user');
+      res.setHeader(
+        'Set-Cookie',
+        cookie.serialize('JWT_token', 'abcdefgjklfskdlf', {
+          httpOnly: true,
+          secure: process.env.NODE_ENV !== 'development',
+          expires: new Date(0), // in the past
+          sameSite: 'strict',
+          path: '/',
+        })
+      );
+      res.status(200).json({ message: 'success' });
+    }
   } catch (err) {
     res.status(401).json(err);
   }
