4726dc320b18ab5c9dca83d42e5a872f2cfda3ef
Bjorn Timmerhuis <bjorn@MacBook-Pro.local>
2021-07-26 08:29:47 +0200

    changed api route

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
-export const API_URL = 'https://api-farmacia-del-mar.herokuapp.com/api/v1/';
-// process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api/v1/';
-export const NEXT_API = 'https://api-farmacia-del-mar.herokuapp.com/api/v1/';
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api/v1/';
+export const NEXT_API = 'https://www.farmaciadelmar.com/api/';
 
 // Public API route for build process: 'https://api-farmacia-del-mar.herokuapp.com/api/v1/';
