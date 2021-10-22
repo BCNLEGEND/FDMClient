93034168a1f3c20c61d60cc53a312373082f5563
Bjorn Timmerhuis <bjorn@MacBook-Pro.local>
2021-10-22 19:35:12 +0200

    made changed to the admin and order page

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useContext} from 'react';
 import { useRouter } from 'next/router';
 import AuthContext from '@/context/AuthContext';
-import { OrderProvider } from '@/context/order';
+import { OrderProvider} from '@/context/OrderContext';
 
 import Grid from '@material-ui/core/Grid';
 import AdminDashboard from '@/components/Admin/AdminDashboard';
