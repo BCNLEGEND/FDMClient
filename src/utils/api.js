28f09decba23b5d905fe4c33359adfc326c03ddc
Bjorn Timmerhuis <bjorn@MacBook-Pro.local>
2021-10-31 08:28:05 +0100

    prepared for testing

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,15 @@
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api/v1/';
+  process.env.NEXT_PUBLIC_API_URL ||
+  'https://api-farmacia-del-mar.herokuapp.com/api/v1/';
 export const NEXT_API =
   process.env.NEXT_PUBLIC_NEXT_URL || 'http://localhost:3000/api/';
 export const IMG_API =
-  process.env.NEXT_PUBLIC_IMG_URL || 'http://localhost:8000';
+  process.env.NEXT_PUBLIC_IMG_URL ||
+  'https://api-farmacia-del-mar.herokuapp.com';
 
 // @TODO In deployment check that the URL direction is correct
 export const IMG_VOTE_API =
   process.env.NEXT_PUBLIC_IMG_VOTE_URL ||
-  'http://localhost:8000/api/v1/photocontestupload/';
+  'https://api-farmacia-del-mar.herokuapp.com/api/v1/photocontestupload/';
 
 // Public API route for build process: 'https://api-farmacia-del-mar.herokuapp.com/api/v1/';
